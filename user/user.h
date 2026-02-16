af49083c9af47559cc630b44c3f31b5fbaee7836
Mohamad Marandi <mohamadmarandi83@gmail.com>
2026-02-16 19:04:57 +0330

    fixing symlink name

--- user/user.h
+++ user/user.h
@@ -24,7 +24,7 @@ int getpid(void);
 char* sys_sbrk(int,int);    
 int pause(int);
 int uptime(void);
-int sys_symlink(char *, char *);
+int symlink(char *, char *);
 
 // ulib.c
 int stat(const char*, struct stat*);
